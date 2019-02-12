924c5077ccbdfbe39cd8ce438b5de1b99d935d0a
Scott Allen <scott@odetocode.com>
2019-02-12 13:34:11 -0500

    starting m3

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -5,7 +5,7 @@ namespace GradeBook
     class Program
     {
         static void Main(string[] args)
-        {            
+        {          
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
