3f4d5321fdd0859c3810f18ad07d79957a4a1118
Scott Allen <scott@odetocode.com>
2019-02-14 15:50:27 -0500

    m3-c07

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,7 +7,6 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {          
-            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
             var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);            
 
@@ -16,7 +15,8 @@ namespace GradeBook
             {
                 result += number;
             } 
-            Console.WriteLine(result);
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
 
             if(args.Length > 0)
             {
