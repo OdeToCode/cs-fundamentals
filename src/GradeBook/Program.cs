efaceff272003c1a2a70c48489e54a404308737c
Scott Allen <scott@odetocode.com>
2019-02-14 15:42:09 -0500

    m03-c06

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,10 +8,14 @@ namespace GradeBook
         static void Main(string[] args)
         {          
             var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);            
 
-            var result = numbers[0];
-            result += numbers[1];
-            result += numbers[2];
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number;
+            } 
             Console.WriteLine(result);
 
             if(args.Length > 0)
