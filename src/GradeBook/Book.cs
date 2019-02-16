916034f96d519b9e647ed51ff10f97a2ab0b40ce
Scott Allen <scott@odetocode.com>
2019-02-16 14:58:55 -0500

    end of m4

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -13,6 +14,24 @@ namespace GradeBook
         public void AddGrade(double grade)
         {
             grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach(var number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            } 
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");            
         }
 
         private List<double> grades;
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,23 +7,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {               
-
             var book = new Book("Scott's Grade Book");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
-            Book.AddGrade(77.5);
-
-            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1); 
-
-            var result = 0.0;
-            foreach(var number in grades)
-            {
-                
-                result += number;
-            } 
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}");
+            book.AddGrade(77.5);
+            book.ShowStatistics();         
         }        
     }
 }
