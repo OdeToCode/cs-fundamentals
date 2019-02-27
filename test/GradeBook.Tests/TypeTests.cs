07d309d8a9d8185a58a9bcb2bb406276d88fddd8
Scott Allen <scott@odetocode.com>
2019-02-27 16:07:43 -0500

    m06-c07

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -5,13 +5,27 @@ namespace GradeBook.Tests
 {    
     public class TypeTests
     {
+         [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);            
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+
         [Fact]
-        public void Test1()
+        public void CanSetNameFromReference()
         {
             var book1 = GetBook("Book 1");
             SetName(book1, "New Name");
 
-            Assert.Equal("Book 1", book1.Name);            
+            Assert.Equal("New Name", book1.Name);            
         }
 
         private void SetName(Book book, string name)
