904e722d112bcab2ac3b9d5eea5982d0ba0857ff
Scott Allen <scott@odetocode.com>
2019-03-08 11:36:48 -0500

    m09-c07

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -19,28 +19,13 @@ namespace GradeBook
         }
     }
 
-    public interface IBook
-    {
-        void AddGrade(double grade);
-        event GradeAddedDelegate GradeAdded;
-        Statistics GetStatistics();
-        string Name { get; }
-    }
-
-    public abstract class Book : NamedObject, IBook
+    public abstract class Book : NamedObject
     {
         public Book(string name) : base(name)
         {
         }
 
-        public event GradeAddedDelegate GradeAdded;
-
         public abstract void AddGrade(double grade);
-
-        public Statistics GetStatistics()
-        {
-            throw new NotImplementedException();
-        }
     }
 
     public class InMemoryBook : Book
@@ -50,30 +35,7 @@ namespace GradeBook
             grades = new List<double>();
             Name = name;
         }
-
-        public void AddGrade(char letter)
-        {
-            switch(letter)
-            {
-                case 'A':
-                    AddGrade(90);
-                    break;
-
-                case 'B':
-                    AddGrade(80);
-                    break;
-
-                case 'C':
-                    AddGrade(70);
-                    break;
-
-                default:
-                    AddGrade(0);
-                    break;
-                
-            }        
-        }
- 
+         
         public override void AddGrade(double grade)
         {            
             if(grade <= 100 && grade >= 0)
@@ -128,10 +90,7 @@ namespace GradeBook
 
                 default:
                     result.Letter = 'F';
-                    break;
-                                    
-                
-                
+                    break;                                                                
             }
 
             return result;
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -22,7 +22,7 @@ namespace GradeBook
             Console.WriteLine($"The letter grade is {stats.Letter}");
         }
 
-        private static void EnterGrades(IBook book)
+        private static void EnterGrades(Book book)
         {
             while (true)
             {
