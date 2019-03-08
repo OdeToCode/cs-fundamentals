e53ed845780e8d60c90a96b2029fabc4490d1112
Scott Allen <scott@odetocode.com>
2019-03-08 16:31:40 -0500

    m09-cend

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -49,13 +49,32 @@ namespace GradeBook
 
         public override void AddGrade(double grade)
         {
-            var writer = File.AppendText($"{Name}.txt");
-            writer.WriteLine(grade);
+            using(var writer = File.AppendText($"{Name}.txt"))
+            {                
+                writer.WriteLine(grade);
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
         }
 
         public override Statistics GetStatistics()
         {
-            throw new NotImplementedException();
+            var result = new Statistics();
+
+            using(var reader = File.OpenText($"{Name}.txt"))
+            {
+                var line = reader.ReadLine();
+                while(line != null)
+                {
+                    var number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
+            }
+
+            return result;
         }
     }
 
@@ -88,42 +107,12 @@ namespace GradeBook
         public override Statistics GetStatistics()
         {
             var result = new Statistics();
-            result.Average = 0.0;            
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
-
+            
             for(var index = 0; index < grades.Count; index += 1)
             {
-                result.Low = Math.Min(grades[index], result.Low);
-                result.High = Math.Max(grades[index], result.High);
-                result.Average += grades[index];
+                result.Add(grades[index]);                            
             }
-            
-            result.Average /= grades.Count;
-
-            switch(result.Average)
-            {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
                 
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-                
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-
-                default:
-                    result.Letter = 'F';
-                    break;                                                                
-            }
-
             return result;
         }
 
--- src/GradeBook/Statistics.cs
+++ src/GradeBook/Statistics.cs
@@ -1,10 +1,60 @@
+using System;
+
 namespace GradeBook
 {
     public class Statistics
     {
-        public double Average;
+        public double Average
+        {
+            get
+            {
+                return Sum / Count;
+            }
+        }
         public double High;
         public double Low;
-        public char Letter;
+
+        public char Letter
+        {
+            get
+            {
+                switch (Average)
+                {
+                    case var d when d >= 90.0:
+                        return 'A';
+
+                    case var d when d >= 80.0:
+                        return 'B';
+
+                    case var d when d >= 70.0:
+                        return 'C';
+
+                    case var d when d >= 60.0:
+                        return 'D';
+
+                    default:
+                        return 'F';
+                }
+            }
+        }
+
+        public double Sum;
+        public int Count;
+
+        public void Add(double number)
+        {
+            Sum += number;
+            Count += 1;
+            Low = Math.Min(number, Low);
+            High = Math.Max(number, High);
+        }
+
+        public Statistics()
+        {
+            Count = 0;
+            Sum = 0.0;
+            High = double.MinValue;
+            Low = double.MaxValue;
+        }
     }
 }
