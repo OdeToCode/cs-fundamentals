6d527060103a1395e5dc895a061f8f43d74afb70
Scott Allen <scott@odetocode.com>
2019-04-23 10:37:52 -0400

    Finish of first module

---
+++ Employees/Core/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    class Employee
+    {
+        int Id;
+        string Name;
+    }
+}
---
+++ Employees/Employees/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Employees
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var name = "Default";
+
+            if (args.Length > 0)
+            {
+                name = args[0];
+            }
+
+            SayHello(name);
+
+            return 1;
+        }
+
+        private static void SayHello(string name)
+        {
+            var hour = DateTime.UtcNow.Hour;
+
+            if (hour < 12)
+            {
+                Console.WriteLine($"Good morning, {name}!");
+            }
+            else if (hour <= 17)
+            {
+                Console.WriteLine($"Good afternoon, {name}!");
+            }
+            else
+            {
+                Console.WriteLine($"Good evening {name}!");
+            }
+        }
+    }
+}
